3c8e5677d27cd68d242ae7d7091581a116829612
Ruslan-Podvysotsky <podvysutsky00@mail.ru>
2018-12-24 19:03:19 +0200

    reviews

--- source/js/app.js
+++ source/js/app.js
@@ -1,5 +1,5 @@
-$(function(){
-    $('.today-navigation a').click(function(){
+$(function () {
+    $('.today-navigation a').click(function () {
         $(this).parents('.today-tabs').find('.today-wrap').addClass('hide');
         $(this).parent().siblings().removeClass('active');
         var id = $(this).attr('href');
@@ -8,3 +8,17 @@ $(function(){
         return false
     });
 });
+
+$('.slide__show').click(function () {
+    $(this).parent().find('.slide__description').toggleClass('active');
+    $(this).toggleClass('active');
+})
+
+
+$(document).ready(function () {
+    $('.slide').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1
+    })
+})
