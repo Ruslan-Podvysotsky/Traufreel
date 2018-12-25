da959d72fffbbddee1ff36540ab4a81051aceb5e
Ruslan-Podvysotsky <podvysutsky00@mail.ru>
2018-12-25 16:01:12 +0200

    finish

--- source/js/app.js
+++ source/js/app.js
@@ -12,13 +12,43 @@ $(function () {
 $('.slide__show').click(function () {
     $(this).parent().find('.slide__description').toggleClass('active');
     $(this).toggleClass('active');
+    $('.slick-next').click(function(){
+        $('.slide__description').removeClass('active');
+        $('.slide__show').removeClass('active');
+    });
+    $('.slick-prev').click(function(){
+        $('.slide__description').removeClass('active');
+        $('.slide__show').removeClass('active');
+    })
+    
 })
+
 
 
 $(document).ready(function () {
     $('.slide').slick({
         infinite: true,
         slidesToShow: 3,
-        slidesToScroll: 1
+        slidesToScroll: 1,
+        variableWidth: true,
+        centerMode: true,
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                infinite: true,
+                centerMode: false
+              }
+            },
+            {
+              breakpoint: 768,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+          ]   
     })
 })
